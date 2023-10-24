88e7a032f2aca9e29dd62288270d7c8fe58b7000
Shadet <kornelrybacki897@gmail.com>
2023-10-24 19:30:53 +0200

    basic functions done

---
+++ core/src/Screens/PlayScreen.java
@@ -0,0 +1,66 @@
+package Screens;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.mygdx.game.RedMan2D;
+
+import javax.swing.*;
+
+public class PlayScreen implements Screen {
+    private RedMan2D game;
+    Texture texture;
+    private OrthographicCamera gamecam;
+    private StretchViewport gamePort;
+
+    public PlayScreen(RedMan2D game){
+        this.game = game;
+        texture = new Texture("badlogic.jpg");
+        gamecam = new OrthographicCamera();
+        gamePort = new StretchViewport(800, 480, gamecam);
+    }
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1, 0, 0 , 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.setProjectionMatrix(gamecam.combined);
+        game.batch.begin();
+        game.batch.draw(texture, 0, 0);
+        game.batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        gamePort.update(width, height);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
