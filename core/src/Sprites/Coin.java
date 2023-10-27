b8f3c77ddae2d933d555a8e9a8557073e306a02c
Shadet <kornelrybacki897@gmail.com>
2023-10-27 23:46:46 +0200

    cleaned up tileset, coinblocks turn into empty coinblocks upon getting hit

--- core/src/Sprites/Coin.java
+++ core/src/Sprites/Coin.java
@@ -3,19 +3,20 @@ package Sprites;
 import Scenes.Hud;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileSet;
 import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.FixtureDef;
-import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 import com.mygdx.game.RedMan2D;
 
 public class Coin extends InteractiveTileObject{
     private Hud hud;
+    private static TiledMapTileSet tileSet;
+    private final int BLANK_COIN = 6;
 
     public Coin(World world, TiledMap map, Rectangle bounds, Hud hud){
         super(world, map, bounds, hud);
         this.hud = hud;
+        tileSet = map.getTileSets().getTileSet("Tileset1");
         fixture.setUserData(this);
         setCategoryFilter(RedMan2D.COIN_BIT);
     }
@@ -23,8 +24,7 @@ public class Coin extends InteractiveTileObject{
     @Override
     public void interactionWithObject() {
         Gdx.app.log("hitting coin", "");
-        setCategoryFilter(RedMan2D.DESTROYED_BIT);
-        getCell().setTile(null);
+        getCell().setTile(tileSet.getTile(BLANK_COIN));
         hud.addScore(200);
     }
 }
